841e685a1c542dc81571db0c2230b0f50a9207df
hoolek77 <szymon.kin@gmail.com>
2021-01-23 13:14:18 +0100

    Created Popup Class that takes 2 arguments in the constructor. First is the content of the popup and second is the class of html element that should active this popup.

---
+++ src/app/components/popup/popup.ts
@@ -0,0 +1,46 @@
+import './popup.css'
+
+export class Popup {
+  popupDivElement: HTMLDivElement
+  closeBtnElement: HTMLDivElement
+  popupOverlayElement: HTMLDivElement
+  popupContentElement: HTMLDivElement
+
+  // in the constructor provide 2 arguments: content of the popup and class of HTML element that should display it after click
+  constructor(
+    protected popupContentHTML: string,
+    protected popupListenerClass: string
+  ) {
+    this.popupDivElement = document.querySelector('.popup') as HTMLDivElement
+    this.closeBtnElement = document.querySelector(
+      '.close-btn'
+    ) as HTMLDivElement
+    this.popupOverlayElement = document.querySelector(
+      '.overlay'
+    ) as HTMLDivElement
+    this.popupContentElement = document.querySelector(
+      '.popup-content'
+    ) as HTMLDivElement
+    this.closeBtnElement.innerText = '&times;'
+    this.popupContentHTML = popupContentHTML
+    document
+      .querySelector(`.${this.popupListenerClass}`)
+      ?.addEventListener('click', () => {
+        this.insertPopupContent()
+        this.popupDivElement.classList.add('active')
+      })
+    this.closeBtnElement.addEventListener('click', () => {
+      this.popupDivElement.classList.remove('active')
+    })
+    this.popupOverlayElement.addEventListener('click', () => {
+      this.popupDivElement.classList.remove('active')
+    })
+  }
+
+  insertPopupContent(): void {
+    this.popupContentElement.innerHTML = `
+      <div class="close-btn">&times;</div>
+      ${this.popupContentHTML}
+    `
+  }
+}
