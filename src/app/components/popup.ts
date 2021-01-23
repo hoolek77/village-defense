96c2d0927bf15d85e1031399b5cd3afa6720c4fb
hoolek77 <szymon.kin@gmail.com>
2021-01-23 21:48:19 +0100

    removed unused code and moved set timeout

--- src/app/components/popup.ts
+++ src/app/components/popup.ts
@@ -25,16 +25,12 @@ export function renderPopup(popupContentHTML: string, closeBtnContent: string) {
 
     closeBtn.addEventListener('click', () => {
       popup.classList.remove('popup-active')
-      setTimeout(() => {
-        removePopup()
-      }, 200)
+      removePopup()
     })
 
     popupOverlayElement.addEventListener('click', () => {
       popup.classList.remove('popup-active')
-      setTimeout(() => {
-        removePopup()
-      }, 200)
+      removePopup()
     })
   }, 10)
 }
@@ -43,60 +39,7 @@ export function removePopup() {
   const popupDivElement = document.querySelector(
     '.popup-container'
   ) as HTMLDivElement
-  document.body.removeChild(popupDivElement)
-}
-
-export class Popup {
-  constructor(
-    protected popupContentHTML: string,
-    protected closeBtnContent: string
-  ) {
-    this.popupContentHTML = popupContentHTML
-    this.closeBtnContent = closeBtnContent
-  }
-
-  renderPopUpContent(): void {
-    const popup = document.createElement('div') as HTMLDivElement
-    popup.classList.add('popup-container')
-    popup.innerHTML = `
-      <div class="overlay"></div>
-        <div class="popup-content">
-          ${this.popupContentHTML}
-          <button class="close__btn--popup">${this.closeBtnContent}</button>
-        </div>
-      </div>
-    `
-    document.body.appendChild(popup)
-
-    popup.classList.add('popup-active')
-
-    const popupOverlayElement = document.querySelector(
-      '.overlay'
-    ) as HTMLDivElement
-
-    const closeBtn = document.querySelector(
-      '.close__btn--popup'
-    ) as HTMLButtonElement
-
-    closeBtn.addEventListener('click', () => {
-      popup.classList.remove('popup-active')
-      setTimeout(() => {
-        this.removePopup()
-      }, 200)
-    })
-
-    popupOverlayElement.addEventListener('click', () => {
-      popup.classList.remove('popup-active')
-      setTimeout(() => {
-        this.removePopup()
-      }, 200)
-    })
-  }
-
-  removePopup(): void {
-    const popupDivElement = document.querySelector(
-      '.popup-container'
-    ) as HTMLDivElement
+  setTimeout(() => {
     document.body.removeChild(popupDivElement)
-  }
+  }, 200)
 }
