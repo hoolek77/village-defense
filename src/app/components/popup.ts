8c8d1ec077340d218623249a1c0fe2c47a68aa5c
hoolek77 <szymon.kin@gmail.com>
2021-01-23 21:40:15 +0100

    removed popup file from separate directory, changed it so its now eporting two functionds instead of a class and fixed exports

--- src/app/components/popup.ts
+++ src/app/components/popup.ts
@@ -1,3 +1,51 @@
+export function renderPopup(popupContentHTML: string, closeBtnContent: string) {
+  const popup = document.createElement('div') as HTMLDivElement
+  popup.classList.add('popup-container')
+  popup.innerHTML = `
+    <div class="overlay"></div>
+    <div class="popup-content">
+      ${popupContentHTML}
+      <button class="close__btn--popup">${closeBtnContent}</button>
+    </div>
+    </div>
+  `
+
+  document.body.appendChild(popup)
+
+  setTimeout(() => {
+    popup.classList.add('popup-active')
+
+    const popupOverlayElement = document.querySelector(
+      '.overlay'
+    ) as HTMLDivElement
+
+    const closeBtn = document.querySelector(
+      '.close__btn--popup'
+    ) as HTMLButtonElement
+
+    closeBtn.addEventListener('click', () => {
+      popup.classList.remove('popup-active')
+      setTimeout(() => {
+        removePopup()
+      }, 200)
+    })
+
+    popupOverlayElement.addEventListener('click', () => {
+      popup.classList.remove('popup-active')
+      setTimeout(() => {
+        removePopup()
+      }, 200)
+    })
+  }, 10)
+}
+
+export function removePopup() {
+  const popupDivElement = document.querySelector(
+    '.popup-container'
+  ) as HTMLDivElement
+  document.body.removeChild(popupDivElement)
+}
+
 export class Popup {
   constructor(
     protected popupContentHTML: string,
