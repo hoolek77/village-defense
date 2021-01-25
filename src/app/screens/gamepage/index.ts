ac9e19f42fd4669f85ac1a6a84df3cae8234aca1
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-01-25 20:47:43 +0100

    Add start game to game page.

--- src/app/screens/gamepage/index.ts
+++ src/app/screens/gamepage/index.ts
@@ -1,8 +1,12 @@
+import { Game } from '../../game'
+
 const quitButton = <HTMLElement>document.querySelector('.quit__game__button')
 const startScreen = <HTMLElement>document.querySelector('.start__screen')
 const gameScreen = <HTMLElement>document.querySelector('.game__screen')
 
 export class GamePage {
+  private game!: Game
+
   private setClosePageEvent() {
     quitButton.addEventListener('click', () => {
       startScreen.classList.remove('start__screen--opened')
@@ -13,5 +17,8 @@ export class GamePage {
 
   runPage() {
     this.setClosePageEvent()
+
+    this.game = new Game()
+    this.game.start()
   }
 }
