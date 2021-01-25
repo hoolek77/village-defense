395b6c3811a1cd983ccc97bc14f34786ba593ff9
Jakub Góra <74857860+GoraJakub@users.noreply.github.com>
2021-01-25 15:50:02 +0100

    Closes #8
    * started working on homepage
    
    * main menu v1
    
    * added every method to one
    
    * fixed logic and file management

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,11 @@
+import { GamePage, HomePage } from './screens'
+
 export class App {
   start() {
     console.log('Starting application...')
+    const homepage = new HomePage()
+    const gamepage = new GamePage()
+    homepage.runPage()
+    gamepage.runPage()
   }
 }
---
+++ src/app/screens/index.ts
@@ -0,0 +1,2 @@
+export * from './gamepage'
+export * from './homepage'
