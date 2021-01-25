25e19a9b4db13e5318d3e43d259b4139d45c8739
aleksandraj036 <61745477+aleksandraj036@users.noreply.github.com>
2021-01-25 18:24:52 +0100

    Closes #12
    * Music added
    
    * Made requested changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,10 @@
+
+import {playAudio} from './utils'
+export class App {
+  start() {
+    console.log('Starting application...');
+    playAudio(0.7);
+
 import { GamePage, HomePage } from './screens'
 
 export class App {
@@ -7,5 +14,6 @@ export class App {
     const gamepage = new GamePage()
     homepage.runPage()
     gamepage.runPage()
+
   }
 }
--- src/app/index.ts
+++ src/app/index.ts
@@ -2,3 +2,6 @@ export * from './app'
 export * from './screens'
 export * from './components'
 
+export * from './utils'
+
+
