f07c4a221b866f8369f313a2c5b84b9dc1029494
Piotr <p.dybowski13@gmail.com>
2021-01-25 20:28:41 +0100

    fixed conflict

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,14 @@
 
-import {playAudio} from './utils'
-export class App {
-  start() {
-    console.log('Starting application...');
-    playAudio(0.7);
-
+import { playAudio } from './utils'
 import { GamePage, HomePage } from './screens'
 
 export class App {
   start() {
     console.log('Starting application...')
+    playAudio(0.7);
     const homepage = new HomePage()
     const gamepage = new GamePage()
     homepage.runPage()
     gamepage.runPage()
-
   }
 }
