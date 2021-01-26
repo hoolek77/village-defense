96516ccb22c8b8c7e56a908a38e705714bad56af
aleksandraj036 <61745477+aleksandraj036@users.noreply.github.com>
2021-01-27 00:31:03 +0100

    Closes #11
    * Added grid and images
    
    * major fixes for the game page
    
    * changed background image for human race
    
    * minor fixes to method names, parameters that they take and order in home page class
    
    Co-authored-by: hoolek77 <szymon.kin@gmail.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
-
 import { playAudio } from './utils'
 import { GamePage, HomePage } from './screens'
 
 export class App {
   start() {
     console.log('Starting application...')
-    playAudio(0.7);
+    playAudio(0.7)
     const homepage = new HomePage()
-    const gamepage = new GamePage()
     homepage.runPage()
-    gamepage.runPage()
   }
 }
--- src/app/screens/gamepage/index.ts
+++ src/app/screens/gamepage/index.ts
@@ -1,17 +1,43 @@
-const quitButton = <HTMLElement>document.querySelector('.quit__game__button')
-const startScreen = <HTMLElement>document.querySelector('.start__screen')
-const gameScreen = <HTMLElement>document.querySelector('.game__screen')
-
 export class GamePage {
+  quitButton: HTMLButtonElement
+  startScreen: HTMLElement
+  gameScreen: HTMLElement
+  middleBox: HTMLDivElement
+  theme: string
+
+  constructor(theme: string) {
+    this.quitButton = document.querySelector(
+      '.quit__game__button'
+    ) as HTMLButtonElement
+    this.startScreen = document.querySelector('.start__screen') as HTMLElement
+    this.gameScreen = document.querySelector('.game__screen') as HTMLElement
+    this.middleBox = document.querySelector('.middle-box') as HTMLDivElement
+    this.theme = theme
+  }
+
+  private setBackgrondImage() {
+    switch (this.theme) {
+      case 'people':
+        this.middleBox.style.backgroundImage = `url('../../../assets/images/villageImages/people-bg.jpg')`
+        break
+      case 'dwarfs':
+        this.middleBox.style.backgroundImage = `url('../../../assets/images/villageImages/dwarf-bg.jpg')`
+        break
+      case 'elfs':
+        this.middleBox.style.backgroundImage = `url('../../../assets/images/villageImages/elf-bg.jpg')`
+    }
+  }
+
   private setClosePageEvent() {
-    quitButton.addEventListener('click', () => {
-      startScreen.classList.remove('start__screen--opened')
-      startScreen.classList.add('start__screen--closed')
-      gameScreen.style.zIndex = '-1'
+    this.quitButton.addEventListener('click', () => {
+      this.startScreen.classList.remove('start__screen--opened')
+      this.startScreen.classList.add('start__screen--closed')
+      this.gameScreen.style.zIndex = '-1'
     })
   }
 
   runPage() {
     this.setClosePageEvent()
+    this.setBackgrondImage()
   }
 }
