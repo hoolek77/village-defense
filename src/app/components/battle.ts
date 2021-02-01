b707f5ab9a6e547576fcfef1a0baa65ab423ee2e
hoolek77 <szymon.kin@gmail.com>
2021-02-01 20:39:53 +0100

    created battle class and added battle gif

---
+++ src/app/components/battle.ts
@@ -0,0 +1,9 @@
+import { createElement } from '../utils/createElement'
+import { Audio } from '../audio'
+
+export class Batlle {
+  private audio: Audio
+  constructor(audio: Audio) {
+    this.audio = audio
+  }
+}
